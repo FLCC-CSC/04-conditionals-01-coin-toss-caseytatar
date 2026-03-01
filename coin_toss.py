a99ed3557b2404f87658612fd61ada251c058dd1
caseytatar <caseytatar@gmail.com>
2026-03-01 21:34:09 +0000

    code for coin_toss

--- coin_toss.py
+++ coin_toss.py
@@ -1,13 +1,39 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+
+# NAME: Casey Tatar
+# DATE: 3/1/2026
+# BRIEF DESCRIPTION:  This program is a simple application that will randomly return Heads or Tails. To calculate, a random number between 1 and 100 (inclusive) is generated and if the number is 51 or greater then Tails is reported. Otherwise Heads is reported.
+
+
+
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
+
+
+
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+# Don't forget to import random!!!!!
+
+import random
+
+def main():
+    coinflipper()
+
+def coinflipper():
+
+    headsortails = random.randint(1, 100) 
+
+    print('===== Coin Flipper =====')
+    
+    if headsortails >= 51: 
+        print('Tails')
+
+    else: print('Heads')
+
+main()
 
 
 
@@ -18,6 +44,9 @@
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
 
 
 ########################################
@@ -44,24 +73,10 @@ Tails
 
 1. What was the hardest part of completing this lab? 
 
+The hardest part of completing this lab was remembering how to use random numbers. 
 
 
 
 
 
-
-'''
-
-########################################
-#            ATTESTATION
-########################################
-'''
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
 '''
